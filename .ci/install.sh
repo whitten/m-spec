2febead8ed1a8fa6978c05d656d4db4542f5cab2
Kaden Thomas <thomas.kaden4@outlook.com>
2019-04-23 23:17:28 -0700

    Fix .travis.yml build

--- .ci/install.sh
+++ .ci/install.sh
@@ -3,9 +3,9 @@
 set -e
 
 # Install LaTeX
-apt install --yes --force-yes texlive texlive-latex texlive-fonts-recommend texlive-fonts-extra
+sudo apt install --yes --force-yes texlive texlive-latex texlive-fonts-recommend texlive-fonts-extra
 
 # Install Pandoc (use rolling release in future?)
-wget "https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb" -O pandoc.deb
-apt install --yes --force-yes ./pandoc.deb
+sudo wget "https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb" -O pandoc.deb
+sudo apt install --yes --force-yes ./pandoc.deb
 
