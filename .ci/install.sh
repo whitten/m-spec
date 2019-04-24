fac6a8050b4dda3746c9dbd6b70a13d49665c12f
Kaden Thomas <thomas.kaden4@outlook.com>
2019-04-23 23:40:50 -0700

    Fix travis build

--- .ci/install.sh
+++ .ci/install.sh
@@ -7,7 +7,7 @@ set -e
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
 echo "deb http://miktex.org/download/ubuntu xenial universe" | sudo tee /etc/apt/sources.list.d/miktex.list
 sudo apt update
-sudo apt install miktex
+sudo apt install miktex texlive-latex-recommended texlive-xetex texlive-fonts-recommended --force-yes --yes
 
 
 # Install Pandoc (use rolling release in future?)
