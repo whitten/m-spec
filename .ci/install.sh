7451dde37a30965054dfc77f99447c4fd251a237
Kaden Thomas <thomas.kaden4@outlook.com>
2019-04-23 23:44:12 -0700

    Fix it

--- .ci/install.sh
+++ .ci/install.sh
@@ -7,7 +7,8 @@ set -e
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
 echo "deb http://miktex.org/download/ubuntu xenial universe" | sudo tee /etc/apt/sources.list.d/miktex.list
 sudo apt update
-sudo apt install miktex texlive-latex-recommended texlive-xetex texlive-fonts-recommended --force-yes --yes
+sudo apt install miktex --force-yes --yes
+sudo miktexsetup finish
 
 
 # Install Pandoc (use rolling release in future?)
