56e9a2923a992629ea1f1937fb4379569ba63d19
Kaden Thomas <thomas.kaden4@outlook.com>
2019-04-23 23:35:14 -0700

    Potentially fix travis with MikTex

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-# Update Package List
-sudo apt update
 
-# Install LaTeX
-sudo apt install --yes --force-yes texlive texlive-latex-extra --no-install-recommends
+# Install MiKTeX
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+sudo apt update
+sudo apt install miktex
+
 
 # Install Pandoc (use rolling release in future?)
 sudo wget "https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb" -O pandoc.deb
