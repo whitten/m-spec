0df6583010abff281ca1cc06b06942e8d9c4e7d5
Kaden Thomas <thomas.kaden4@outlook.com>
2019-04-23 23:29:02 -0700

    Fix travis build

--- .ci/install.sh
+++ .ci/install.sh
@@ -6,7 +6,7 @@ set -e
 sudo apt update
 
 # Install LaTeX
-sudo apt install --yes --force-yes texlive
+sudo apt install --yes --force-yes texlive texlive-latex-extra --no-install-recommends
 
 # Install Pandoc (use rolling release in future?)
 sudo wget "https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb" -O pandoc.deb
